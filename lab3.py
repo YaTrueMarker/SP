39e63105aee67f77842a86d9f73343b615c139fe
YaTrueMarker <markalyanov@mail.ru>
2023-04-22 15:34:07 +0800

    restck

--- lab3.py
+++ lab3.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'http://biik.ru/rasp/cg109.htm' #website 
-resp = requests.get(url) #get all from site ()
-resp.encoding = 'windows-1251' #because site biik.ru rasp use cp-1251 need do that 
-print(resp.text) #write site answer code
+url = 'http://biik.ru/rasp/cg109.htm'  
+resp = requests.get(url) #получение с сайта всего
+resp.encoding = 'windows-1251' 
+print(resp.text) 
